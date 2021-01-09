10e2bf42683a6d35321fe6595ed4ceb02231384b
Christopher Chee <ketyung@techchee.com>
2021-01-09 08:59:17 +0800

    Initial Commit

---
+++ SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Chee Ket Yung on 09/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
